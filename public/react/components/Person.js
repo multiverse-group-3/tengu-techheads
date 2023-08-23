73f448af340315a426cc8e01dc7a468ae8d9579d
KateEHowlett <kate.howlett@norfolk.gov.uk>
2023-08-23 11:22:16 +0100

    updated database

--- public/react/components/Person.js
+++ public/react/components/Person.js
@@ -7,6 +7,7 @@ export const Person = (props) => {
     <h3>{props.person.name}</h3>
     <p>{props.person.company}</p>
     <p>{props.person.role}</p>
+    <img src={props.person.image} width="200" height="200" />
   </div>
   </>
 } 
--- server/routes/people.js
+++ server/routes/people.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Person } = require("../models");
 
-// GET /Person
+// GET /people
 router.get("/", async (req, res, next) => {
   try {
     const people = await Person.findAll();
@@ -11,5 +11,16 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+// GET a person
+router.get("/:id", async (req, res, next) => {
+  const personId = req.params.id;
+  try {
+    const person = await Person.findByPk(personId);
+    res.send(person);
+  } catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
